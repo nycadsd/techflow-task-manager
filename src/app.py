783427c0c38ad5d9a26253bc6b0887a8ab2f6346
nycadsd <nycoledantas00@gmail.com>
2026-06-03 01:01:03 -0300

    feat: implementa cadastro de tarefas

--- src/app.py
+++ src/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+tarefas = []
 
 @app.route("/")
 def inicio():
     return "Sistema de Gerenciamento de Tarefas - TechFlow Solutions"
+
+
+@app.route("/tarefas", methods=["POST"])
+def criar_tarefa():
+
+    dados = request.get_json()
+
+    tarefa = {
+        "titulo": dados["titulo"]
+    }
+
+    tarefas.append(tarefa)
+
+    return jsonify({
+        "mensagem": "Tarefa criada com sucesso",
+        "tarefa": tarefa
+    }), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
